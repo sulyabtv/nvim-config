cf669dc686fd3bab0a3fb8e29a18d38ad617931f
Sulyab Thottungal Valapu <sulyabtv@gmail.com>
2026-06-27 12:56:59 -0700

    yank file path

--- lua/options.lua
+++ lua/options.lua
@@ -102,3 +102,17 @@ vim.api.nvim_create_autocmd({ 'VimLeave', 'VimSuspend' }, {
   group = tmuxgrp,
   callback = function() tmux_status 'on' end,
 })
+
+-- yank absolute path of current file
+vim.keymap.set('n', '<leader>yp', function()
+  local p = vim.fn.expand '%:p'
+  vim.fn.setreg('+', p)
+  vim.notify(p)
+end, { desc = 'Yank absolute path' })
+
+-- yank path relative to cwd
+vim.keymap.set('n', '<leader>yr', function()
+  local p = vim.fn.expand '%:.'
+  vim.fn.setreg('+', p)
+  vim.notify(p)
+end, { desc = 'Yank relative path' })
